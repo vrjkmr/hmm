c83ab81f3b18a18ca3e6fcbd68e4e1d1eff92a14
Vivian Rajkumar <vivian.rajkumar@hotmail.com>
2020-09-09 13:17:34 +0530

    Added likelihood and decoding

--- hmm.py
+++ hmm.py
@@ -42,11 +42,14 @@ class HMM:
         """Compute observation likelihood using the Forward algorithm."""
         T = len(obs)
         alpha = np.zeros((self.N, T))
+        # initialization
         o_0 = self._get_observation_idx(obs[0])
         alpha[:, 0] = self.initial * self.B[:, o_0]
+        # recursion
         for t in range(1, T):
             o_t = self._get_observation_idx(obs[t])
             alpha[:, t] = alpha[:, t-1].dot(self.A) * self.B[:, o_t]
+        # termination
         prob = alpha[:, T-1].sum()
         return prob, alpha
 
@@ -54,10 +57,13 @@ class HMM:
         """Compute observation likelihood using the Backward algorithm."""
         T = len(obs)
         beta = np.zeros((self.N, T))
+        # initialization
         beta[:, T-1] = 1
+        # recursion
         for t in range(T-2, -1, -1):
             o_t1 = self._get_observation_idx(obs[t+1])
             beta[:, t] = self.A.dot(self.B[:, o_t1] * beta[:, t+1])
+        # termination
         o_0 = self._get_observation_idx(obs[0])
         prob = self.initial.dot(self.B[:, o_0] * beta[:, 0])
         return prob, beta
@@ -66,12 +72,15 @@ class HMM:
         """Determine the best hidden state sequence given an observation."""
         T = len(obs)
         delta = np.zeros((self.N, T))
+        # initialization
         o_0 = self._get_observation_idx(obs[0])
         delta[:, 0] = self.initial * self.B[:, o_0]
+        # recursion
         for t in range(1, T):
             o_t = self._get_observation_idx(obs[t])
             delta_prev = delta[:, t-1].reshape(-1, 1)
             delta[:, t] = (delta_prev * self.A).max(axis=0) * self.B[:, o_t]
+        # termination
         best_path = self.Q[delta.argmax(axis=0)]
         prob = delta[:, T-1].max()
         return best_path, prob, delta
